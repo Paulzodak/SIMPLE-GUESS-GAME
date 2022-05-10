960152cdc2341fcac88bd971ade77bc124497208
Zodak <ojepaul4jesus@gmail.com>
2022-05-10 19:08:03 +0100

    finished desktop page

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-const randomNumber = Math.round(Math.random() * 10);
-let scorecounter = 10;
+let randomNumber = Math.round(Math.random() * 10);
+let livesCounter = 3;
+let liveCounterMessage = (document.querySelector(
+  ".score"
+).textContent = `Lives : ${livesCounter}`);
+console.log(liveCounterMessage);
+document.querySelector(".Highscore").textContent = `Highscore : ${0}`;
+console.log(liveCounterMessage);
+// let wrongAnswerAnimation = (document.querySelector(
+//   ".again"
+// ).style.animationName = "answer_container_animation");
 const getUserInput = () => {
+  let isLiveCounterGreaterThanZero = livesCounter > 0;
+  let isLiveCounterLesserThanZero = livesCounter < 0;
   // Getting user input
   const userInput = document.querySelector(".userInput").value;
   // Convert user input to number to avoid bugs
@@ -9,83 +20,112 @@ const getUserInput = () => {
   // console.log(randomNumber);
   if (userInput < 11 && userInput > 0) {
     console.log(randomNumber);
-    if (userInputToNumber === randomNumber && scorecounter > 0) {
+
+    // CONDITION 1
+    if (userInputToNumber === randomNumber && isLiveCounterGreaterThanZero) {
       document.querySelector(".answer_container").innerHTML =
         randomNumber + "!";
       document.querySelector(".answer_container").style.backgroundColor =
         "green";
-      document.querySelector(".hinter").innerHTML = `Yes , You did it!!`;
+      document.querySelector(".hinter").innerHTML = `Yes!,You did it!!`;
       document.querySelector(".answer_container").style.animationName = "none";
-    } else if (
-      userInputToNumber === randomNumber + 2 ||
-      userInputToNumber === randomNumber + 1 ||
-      userInputToNumber === randomNumber - 2 ||
-      (userInputToNumber === randomNumber - 1 && scorecounter > 0)
-    ) {
-      scorecounter--;
-      document.querySelector(".again").style.animationName =
-        "answer_container_animation";
-      console.log(`score counter : ${scorecounter}`);
-      document.querySelector(".hinter").innerHTML = "You are getting closer";
-    } else if (
-      userInputToNumber === randomNumber + 3 ||
-      userInputToNumber === randomNumber + 4 ||
-      (userInputToNumber === randomNumber + 5 && scorecounter > 0)
-    ) {
-      scorecounter--;
-      document.querySelector(".again").style.animationName =
-        "answer_container_animation";
-      console.log(`score counter : ${scorecounter}`);
-      document.querySelector(".hinter").innerHTML = "Too High";
+      document.querySelector(
+        ".Highscore"
+      ).innerHTML = `Highscore : ${livesCounter}`;
+      console.log(`highscore : ${livesCounter}`);
+      document.querySelector("button").style.display = "none";
+      document.querySelector(".guess").style.animationName = "blink_animation";
     }
-    // document.querySelector(".score").innerHTML = `Score : ${(liveCounter =
-    //   liveCounter - 1)}`;
+    // CONDITION 2
+    else if (userInput !== randomNumber && isLiveCounterGreaterThanZero) {
+      livesCounter--;
+      let liveCounterMessage = (document.querySelector(
+        ".score"
+      ).textContent = `Lives : ${livesCounter}`);
+      if (
+        userInputToNumber === randomNumber + 2 ||
+        userInputToNumber === randomNumber + 1 ||
+        userInputToNumber === randomNumber - 2 ||
+        userInputToNumber === randomNumber - 1
+      ) {
+        document.querySelector(".again").style.animationName =
+          "answer_container_animation";
+        liveCounterMessage;
+        document.querySelector(".hinter").innerHTML = "You are getting closer";
+      } else if (
+        userInputToNumber === randomNumber + 3 ||
+        userInputToNumber === randomNumber + 4 ||
+        userInputToNumber === randomNumber + 5
+      ) {
+        document.querySelector(".again").style.animationName =
+          "answer_container_animation";
+        liveCounterMessage;
+        document.querySelector(".hinter").innerHTML = "Too High";
+      } else if (
+        userInputToNumber === randomNumber - 3 ||
+        userInputToNumber === randomNumber - 4 ||
+        userInputToNumber === randomNumber - 5
+      ) {
+        document.querySelector(".again").style.animationName =
+          "answer_container_animation";
+        liveCounterMessage;
+        document.querySelector(".hinter").innerHTML = "Too Low !!!";
+      } else if (
+        userInputToNumber === randomNumber + 6 ||
+        userInputToNumber === randomNumber + 7 ||
+        userInputToNumber === randomNumber + 8 ||
+        userInputToNumber === randomNumber + 9 ||
+        userInputToNumber === randomNumber + 10
+      ) {
+        document.querySelector(".again").style.animationName =
+          "answer_container_animation";
+        liveCounterMessage;
+        document.querySelector(".hinter").innerHTML = "Try Harder !!!";
+      } else if (
+        userInputToNumber === randomNumber - 6 ||
+        userInputToNumber === randomNumber - 7 ||
+        userInputToNumber === randomNumber - 8 ||
+        userInputToNumber === randomNumber - 9 ||
+        userInputToNumber === randomNumber - 10
+      ) {
+        document.querySelector(".again").style.animationName =
+          "answer_container_animation";
+        liveCounterMessage;
+        document.querySelector(".hinter").innerHTML = "Try Harder !!!";
+      } else if (isLiveCounterLesserThanZero) {
+        console.log("Game over");
+      }
+    } // END OF CONDITION 2
     else if (
-      userInputToNumber === randomNumber - 3 ||
-      userInputToNumber === randomNumber - 4 ||
-      (userInputToNumber === randomNumber - 5 && scorecounter > 0)
+      userInput !== randomNumber ||
+      userInput === randomNumber ||
+      isLiveCounterLesserThanZero
     ) {
-      scorecounter--;
-      document.querySelector(".again").style.animationName =
-        "answer_container_animation";
-      console.log(`score counter : ${scorecounter}`);
-      document.querySelector(".hinter").innerHTML = "Too Low !!!";
-    }
-    // document.querySelector(".score").innerHTML = `Score : ${(liveCounter =
-    //   liveCounter - 1)}`;
-    else if (
-      userInputToNumber === randomNumber + 6 ||
-      userInputToNumber === randomNumber + 7 ||
-      userInputToNumber === randomNumber + 8 ||
-      userInputToNumber === randomNumber + 9 ||
-      (userInputToNumber === randomNumber + 10 && scorecounter > 0)
-    ) {
-      scorecounter--;
-      document.querySelector(".again").style.animationName =
-        "answer_container_animation";
-      console.log(`score counter : ${scorecounter}`);
-      document.querySelector(".hinter").innerHTML = "Try Harder !!!";
-    } else if (
-      userInputToNumber === randomNumber - 6 ||
-      userInputToNumber === randomNumber - 7 ||
-      userInputToNumber === randomNumber - 8 ||
-      userInputToNumber === randomNumber - 9 ||
-      (userInputToNumber === randomNumber - 10 && scorecounter > 0)
-    ) {
-      scorecounter--;
-      document.querySelector(".again").style.animationName =
-        "answer_container_animation";
-      console.log(`score counter : ${scorecounter}`);
-      document.querySelector(".hinter").innerHTML = "Try Harder !!!";
-    } else {
-      document.querySelector(".hinter").innerHTML =
-        "CHOOSE A DIGIT BETWEEN 1 -10!!";
+      document.querySelector("button").style.display = "none";
+      document.querySelector(".hinter").innerHTML = `Game over!`;
     }
   } else {
-    console.log("ghjuiop");
+    document.querySelector(".hinter").innerHTML =
+      "CHOOSE A DIGIT BETWEEN 1 -10!!";
   }
 };
 
 const hover = () => {
   document.querySelector(".again").style.animationName = "none";
+};
+const reset = () => {
+  livesCounter = 3;
+  randomNumber = Math.round(Math.random() * 10);
+  // document.querySelector(".answer_container").innerHTML = randomNumber + "!";
+  // document.querySelector(".answer_container").style.backgroundColor = "green";
+  // document.querySelector(".hinter").innerHTML = `Yes!,You did it!!`;
+  // document.querySelector(".answer_container").style.animationName = "none";
+  document.querySelector(".Highscore").innerHTML = `Highscore : ${0}`;
+  document.querySelector(".hinter").innerHTML = ` `;
+  let liveCounterMessage = (document.querySelector(
+    ".score"
+  ).textContent = `Lives : ${livesCounter}`);
+  // console.log(`highscore : ${livesCounter}`);
+  document.querySelector("button").style.display = "block";
+  // document.querySelector(".guess").style.animationName = "blink_animation";
 };
